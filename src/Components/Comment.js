6fedeea1a34cc8e53b40010643208c5573030c19
Brian Ruff <thelonewolf198927@gmail.com>
2019-10-12 03:22:28 -0400

    styling for comment

--- src/Components/Comment.js
+++ src/Components/Comment.js
@@ -3,13 +3,18 @@ import React from "react";
 export default class Comment extends React.Component {
   render() {
     return (
-      <div className="comment-container">
-        <h4 className="posted-by">{this.props.comment.author}</h4>
+      <div style={{ border: "1px solid black" }} className="comment-container">
+        <h4 className="posted-by">Author{this.props.comment.author}</h4>
         <p>{this.props.comment.text}</p>
         <div className="post-info">
           {" "}
-          <span className="likes">{this.props.comment.likes}</span>
-          <span className="dislikes">{this.props.comment.dislikes}</span>
+          <span style={{ margin: "0 1.6rem" }} className="likes">
+            {this.props.comment.likes}
+          </span>{" "}
+          ||
+          <span style={{ margin: "0 1.6rem" }} className="dislikes">
+            {this.props.comment.dislikes}
+          </span>
         </div>
       </div>
     );
