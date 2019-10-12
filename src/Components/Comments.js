1b0763018afbaa1a2a8bd62b61518b420ce67689
Brian Ruff <thelonewolf198927@gmail.com>
2019-10-12 01:52:28 -0400

    error handling for comments

--- src/Components/Comments.js
+++ src/Components/Comments.js
@@ -2,32 +2,57 @@ import React from "react";
 import "../styles/comments.css";
 import axios from "axios";
 import Comment from "../Components/Comment";
+import { ClipLoader } from "react-spinners";
 
 export default class Comments extends React.Component {
   state = {
-    comments: []
+    comments: [],
+    hasError: false,
+    status: "Loading"
   };
   componentDidMount() {
     axios
       .get(
         `https://suicide-watch-backend.herokuapp.com/comments/article/${this.props.article.id}`
       )
-      .then(res => console.log(res.data))
-      .catch(err => console.log(err));
+      .then(res =>
+        this.setState({
+          comments: res.data,
+          status: "Complete",
+          hasError: false
+        })
+      )
+      .catch(err =>
+        this.setState(
+          { comments: [], hasError: true, status: "Complete" },
+          () => console.log(JSON.stringify(err, null, 4))
+        )
+      );
   }
   render() {
-    return (
-      <React.Fragment>
-        <ul className="comment-list">
-          {this.state.comments.length < 1 ? (
-            <h4>No Comments :(</h4>
+    if (this.state.hasError) {
+      return <h4>Error Loading Page :(</h4>;
+    } else {
+      return (
+        <React.Fragment>
+          {this.state.status === "Complete" ? (
+            <React.Fragment>
+              <h4>Loading</h4>
+              <ClipLoader />
+            </React.Fragment>
           ) : (
-            (this.state.comments.map || []).map(comment => {
-              return <Comment comment={comment} />;
-            })
+            <ul className="comment-list">
+              {this.state.comments.length < 1 ? (
+                <h4>No Comments :(</h4>
+              ) : (
+                (this.state.comments.map || []).map(comment => {
+                  return <Comment comment={comment} />;
+                })
+              )}
+            </ul>
           )}
-        </ul>
-      </React.Fragment>
-    );
+        </React.Fragment>
+      );
+    }
   }
 }
