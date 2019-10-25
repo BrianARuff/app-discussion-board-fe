9efc643ee9dded2f5cce15f691b7ddf7382726a2
brianaruff <briananthonyruff@gmail.com>
2019-10-25 03:24:17 -0400

    added test for non ajax register button functionality via react-dom testing tools and friends

--- src/__tests__/Register.spec.js
+++ src/__tests__/Register.spec.js
@@ -1,10 +1,40 @@
 import React from "react";
-import { create } from "react-test-renderer";
+import { create, act } from "react-test-renderer";
 import Register from "../Pages/Register.js";
+import ReactDOM from "react-dom";
+import { act as domAct } from "react-dom/test-utils";
+import axios from "axios";
+
+let container;
+
+beforeEach(() => {
+  container = document.createElement("div");
+  document.body.appendChild(container);
+});
+
+afterEach(() => {
+  document.body.removeChild(container);
+  container = null;
+});
 
 describe("test register component", () => {
   test("for snapshot testing", () => {
     const register = create(<Register />);
     expect(register.toJSON()).toMatchSnapshot();
+  });
+
+  test("register button default text to be 'Register'", () => {
+    ReactDOM.render(<Register />, container);
+    const button = container.querySelectorAll("span")[3];
+    expect(button.textContent).toBe("Register");
+  });
+
+  test("register button text changes to 'Registering' after clicking Register button", () => {
+    ReactDOM.render(<Register />, container);
+    const registerButton = container.querySelectorAll("span")[3];
+    domAct(() => {
+      registerButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
+    });
+    expect(registerButton.textContent).toBe("Registering");
   });
 });
